c6d9129bdef9d24c72ead6e7db66c59f521ed9ad
John-Mark Bell <jmb@pexip.com>
2017-04-18 23:01:58 +0000

    Imported Upstream version 1.4.4

--- django_nose/management/commands/test.py
+++ django_nose/management/commands/test.py
@@ -1,24 +1,14 @@
+# coding: utf-8
 """
-Add extra options from the test runner to the ``test`` command, so that you can
-browse all the nose options from the command line.
+Add extra options from the test runner to the ``test`` command.
+
+This enables browsing all the nose options from the command line.
 """
+from __future__ import unicode_literals
+
 from django.conf import settings
+from django.core.management.commands.test import Command
 from django.test.utils import get_runner
-
-
-if 'south' in settings.INSTALLED_APPS:
-    from south.management.commands.test import Command
-else:
-    from django.core.management.commands.test import Command
-
-
-# Django < 1.2 compatibility
-test_runner = settings.TEST_RUNNER
-if test_runner.endswith('run_tests') or test_runner.endswith('run_gis_tests'):
-    import warnings
-    warnings.warn(
-        'Use `django_nose.NoseTestSuiteRunner` instead of `%s`' % test_runner,
-        DeprecationWarning)
 
 
 TestRunner = get_runner(settings)
@@ -30,4 +20,6 @@ else:
 
 
 class Command(Command):
-    option_list = Command.option_list + tuple(extra_options)
+    """Implement the ``test`` command."""
+
+    option_list = getattr(Command, 'option_list', ()) + tuple(extra_options)
